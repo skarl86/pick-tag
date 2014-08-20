14e6091c88fbfd1a9eed572be140a202f5f44284
NK <beohemian@gmail.com>
2014-08-20 12:43:48 +0900

    Initial Commit

---
+++ COSMASTER/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  COSMASTER
+//
+//  Created by NCri on 2014. 8. 20..
+//  Copyright (c) 2014년 TFD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ COSMASTER/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  COSMASTER
+//
+//  Created by NCri on 2014. 8. 20..
+//  Copyright (c) 2014년 TFD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
